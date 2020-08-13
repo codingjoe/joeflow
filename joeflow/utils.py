e85d0a9430607070e8af720112b04145eb595efb
Johannes Hoppe <info@johanneshoppe.com>
2020-08-13 22:03:52 +0200

    Display if an edge was traveled or not and add all edges

--- joeflow/utils.py
+++ joeflow/utils.py
@@ -1,4 +1,5 @@
 import types
+from collections import defaultdict
 
 import graphviz as gv
 
@@ -25,18 +26,64 @@ def get_workflow(name):
 
 
 class NoDashDiGraph(gv.Digraph):
-    """Like `.graphviz.Digraph` but removes underscores from labels."""
+    """
+    Like `.graphviz.Digraph` but with unique nodes and edges.
+
+    Nodes and edges are unique and their attributes will be overridden
+    should the same node or edge be added twice. Nodes are unique by name
+    and edges unique by head and tail.
+
+    Underscores are replaced with whitespaces from identifiers.
+    """
 
     def __init__(self, *args, **kwargs):
-        self._edges = []
+        self._nodes = defaultdict(dict)
+        self._edges = defaultdict(dict)
         super().__init__(*args, **kwargs)
 
-    def edge(self, tail_name, head_name, label=None, _attributes=None, **attrs):
-        if not (tail_name, head_name) in self._edges:
-            self._edges.append((tail_name, head_name))
-            super().edge(
-                tail_name, head_name, label=label, _attributes=_attributes, **attrs
-            )
+    def __iter__(self, subgraph=False):
+        """Yield the DOT source code line by line (as graph or subgraph)."""
+        if self.comment:
+            yield self._comment % self.comment
+
+        if subgraph:
+            if self.strict:
+                raise ValueError("subgraphs cannot be strict")
+            head = self._subgraph if self.name else self._subgraph_plain
+        else:
+            head = self._head_strict if self.strict else self._head
+        yield head % (self._quote(self.name) + " " if self.name else "")
+
+        for kw in ("graph", "node", "edge"):
+            attrs = getattr(self, "%s_attr" % kw)
+            if attrs:
+                yield self._attr % (kw, self._attr_list(None, attrs))
+
+        yield from self.body
+
+        for name, attrs in sorted(self._nodes.items()):
+            name = self._quote(name)
+            label = attrs.pop("label", None)
+            _attributes = attrs.pop("_attributes", None)
+            attr_list = self._attr_list(label, attrs, _attributes)
+            yield self._node % (name, attr_list)
+
+        for edge, attrs in sorted(self._edges.items()):
+            head_name, tail_name = edge
+            tail_name = self._quote_edge(tail_name)
+            head_name = self._quote_edge(head_name)
+            label = attrs.pop("label", None)
+            _attributes = attrs.pop("_attributes", None)
+            attr_list = self._attr_list(label, attrs, _attributes)
+            yield self._edge % (head_name, tail_name, attr_list)
+
+        yield self._tail
+
+    def node(self, name, **attrs):
+        self._nodes[name] = attrs
+
+    def edge(self, tail_name, head_name, **attrs):
+        self._edges[(tail_name, head_name)] = attrs
 
     @staticmethod
     def _quote(identifier, *args, **kwargs):
