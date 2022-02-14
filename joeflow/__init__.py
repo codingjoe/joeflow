878578e08e1aa30ea8d469985939122c3724a0e6
Rustem Saiargaliev <rustem.saiargaliev@voiio.de>
2022-02-14 11:19:07 +0100

    Sync active Python/Django versions
    - Python: add 3.9, 3.10, remove 3.6
    - Django: add 4.0, remove 3.0, 3.1
    - fixes to PyPI classifiers

--- joeflow/__init__.py
+++ joeflow/__init__.py
@@ -1,2 +1,5 @@
 """The lean workflow automation framework for machines with heart."""
-default_app_config = "joeflow.apps.JoeflowConfig"
+import django
+
+if django.VERSION < (3, 2):
+    default_app_config = "joeflow.apps.JoeflowConfig"
