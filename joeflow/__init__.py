0b1c2457547d6d5c4490594aa653596ba298ecc7
Rust Saiargaliev <fly.amureki@gmail.com>
2022-10-28 10:38:34 +0200

    [draft] Attempt to move to pyproject.toml

--- joeflow/__init__.py
+++ joeflow/__init__.py
@@ -1,5 +1,10 @@
 """The lean workflow automation framework for machines with heart."""
 import django
 
+from . import _version
+
+__version__ = _version.version
+VERSION = _version.version_tuple
+
 if django.VERSION < (3, 2):
     default_app_config = "joeflow.apps.JoeflowConfig"
--- setup.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup
-
-setup(name="joeflow", use_scm_version=True)
