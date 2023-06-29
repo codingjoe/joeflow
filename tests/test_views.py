d75d27c7efcfb6f8c550356f8d2154a42b6348fb
Rust Saiargaliev <fly.amureki@gmail.com>
2023-06-29 14:28:49 +0200

    Fix #81 -- Finish machine tasks (#83)

--- tests/test_views.py
+++ tests/test_views.py
@@ -22,11 +22,15 @@ class TestTaskViewMixin:
         assert response.status_code == 302
 
         wf = workflows.SimpleWorkflow.objects.get()
-
         assert wf.task_set.count() == 2
+
+        start_task = wf.task_set.get(name="start_view")
+        assert start_task.type == "human"
+        assert start_task.status == "succeeded"
+
         new_task = wf.task_set.get(name="save_the_princess")
-        assert new_task.name == "save_the_princess"
         assert new_task.type == "human"
+        assert new_task.status == "scheduled"
         assert not new_task.assignees.all()
 
     def test_custom_create_task(self, db, admin_client, admin_user):
@@ -35,7 +39,7 @@ class TestTaskViewMixin:
         assert response.status_code == 302
 
         wf = workflows.AssigneeWorkflow.objects.get()
-
         assert wf.task_set.count() == 2
+
         new_task = wf.task_set.get(name="save_the_princess")
         assert admin_user in new_task.assignees.all()
